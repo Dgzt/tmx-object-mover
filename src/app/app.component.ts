7edb1e7a640833e54d607fe2b84fa6ab6ca79b05
Zsuró Tibor <zsurotibor@gmail.com>
2020-11-24 17:05:58 +0100

    Add states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StateEnum} from './enum/state.enum';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  public StateEnum = StateEnum;
+
+  public state: StateEnum;
+
+  ngOnInit(): void {
+    this.state = StateEnum.UPLOAD;
+  }
 
   public upload(event: any): void {
     const file = event.files[0];
@@ -14,6 +23,7 @@ export class AppComponent {
     const fileReader = new FileReader();
     fileReader.onload = (e) => {
       console.log(fileReader.result);
+      this.state = StateEnum.PARAMETERS;
     };
 
     fileReader.readAsText(file);
